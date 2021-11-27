33371661b482d40ed19d0122795f3d5ffb4465bf
Bartek <lizusfull@gmail.com>
2021-11-27 11:32:29 +0100

    user role authentication

--- node-server/routes/users.js
+++ node-server/routes/users.js
@@ -1,10 +1,84 @@
 import bcrypt from 'bcrypt';
 import express from 'express';
 import passport from 'passport';
-import dbManager from ".././lib/db.js";
-// import { ensureAuthenticated, forwardAuthenticated } from ".././config/auth.js";
+import {newUser,userRole} from ".././lib/db.js";
+import { ensureAuthenticated, forwardAuthenticated } from ".././config/auth.js";
 const router = express.Router();
 
+var requireAuth = passport.authenticate('jwt', { session: false }),
+  requireLogin = passport.authenticate('local', { session: false });
+
+  function roleAuthorization (roles) {
+    return function (req, res, next) {
+      var user = req.user;
+  
+      // User.findById(user._id, function (err, foundUser) {
+      //   if (err) {
+      //     res.status(422).json({ error: 'No user found.' });
+      //     return next(err);
+      //   }
+  
+      //   if (roles.indexOf(foundUser.role) > -1) {
+      //     return next();
+      //   }
+  
+      //   res
+      //     .status(401)
+      //     .json({ error: 'You are not authorized to view this content' });
+      //   return next('Unauthorized');
+      // });
+      return next("nice");
+    };
+  };
+
+router.use("/protected",async function getUserRoles(req, res, next) {
+  req.userRoleNames = [];
+
+  if (req.isAuthenticated()) {
+    req.userRoleNames.push('authenticated');
+    // console.log("ses",req.session.passport.user)
+    if(req.session?.passport?.user){
+      const {err,role} = await userRole(req.session.passport.user);
+      req.userRoleNames.push(role);
+    }
+  } else {
+    req.userRoleNames.push('unAuthenticated');
+    // return next(); // skip role load if dont are authenticated
+  }
+
+  // get user roles, you may get roles from DB ...
+  // and if are admin add its role
+  // console.log(req.session)
+  
+  // req.userRoleNames.push('administrator');
+
+  next();
+
+});
+  /**
+ * @swagger
+ * /users/protected:
+ *   get:
+ *       descripton: Returns list of stops in path and total distance.
+ *       content: 
+ *          application/x-www-form-urlencoded
+ *       parameters:
+ *       responses:
+ *           200:
+ *               description: Sucesfull login
+ *           401:
+ *               description: Cant login
+ *               content:
+ *                   application/json:
+ *               schema:
+ *                   type: object
+ */
+router.get('/protected', function (req, res) {
+  // if(req.userRoleNames[])
+  console.log(req.userRoleNames);
+  // console.log(req.session);
+  res.send({ content: 'Success' });
+});
 // router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
 // router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
 
@@ -16,18 +90,10 @@ const router = express.Router();
  *       content: 
  *          application/x-www-form-urlencoded
  *       parameters:
- *          - in: query
- *            name: email
- *            default: a@a
- *            required: true
+ *          - in: body
+ *            name: aa
  *            schema:
- *              type: string
- *          - in: query
- *            name: password
- *            default: PH6bGEXDTd6RZ8h
- *            required: true
- *            schema:
- *              type: string
+ *              $ref: '#/definitions/User'
  *       responses:
  *           200:
  *               description: Sucesfull login
@@ -38,8 +104,8 @@ const router = express.Router();
  *               schema:
  *                   type: object
  */
-router.post('/register', (req, res) => {
-  const { email, password } = req.query;
+router.post('/register', async(req, res) => {
+  const { email, password } = req.body;
   const name = "a";
   const password2 = password;
   let errors = [];
@@ -60,38 +126,19 @@ router.post('/register', (req, res) => {
     res.status(401).send(errors)
   }
   else {
-    register(res, name, email, password, password2).then(() => {
-    })
+    const hash = await hashPassword(password);
+    const  {err,user} = await newUser(name, email, hash);
+    if(err){
+      res.status(401).send({ msg: err.message });
+    }
+    else{
+      res.status(200).send({msg:"Added"})
+    }
+    // await register(res, name, email, password, password2).then(() => {
+    // })
   }
 });
-async function register(res, name, email, password, password2) {
-  let errors = []
-  try {
-    const db = await dbManager.db();
-    const userCollection = db.collection(`Users`);
-    if (await userCollection.findOne({ email: email }) != null) {
-      errors.push({ msg: 'Email already exists' });
-      res.status(401).send(errors)
-    }
-    else {
-      const newUser = new User(
-        name,
-        email,
-        password
-      );
-      const hash = await hashPassword(newUser);
-      newUser.password = hash;
-      await userCollection.insertOne(newUser);
-      await res.status(200).send({ msg: "Succesfully created" });
-      return 0;
-    }
-  } catch (e) {
-    console.log("e", e);
-  }
-}
-async function hashPassword(user) {
-
-  const password = user.password
+async function hashPassword(password) {
   const saltRounds = 10;
 
   // bcrypt.hash(password, saltRounds, function(err, hash) {
@@ -119,18 +166,10 @@ async function hashPassword(user) {
  *       content: 
  *          application/x-www-form-urlencoded
  *       parameters:
- *          - in: query
- *            name: email
- *            default: a@a
- *            required: true
+ *          - in: body
+ *            name: aa
  *            schema:
- *              type: string
- *          - in: query
- *            name: password
- *            default: PH6bGEXDTd6RZ8h
- *            required: true
- *            schema:
- *              type: string
+ *              $ref: '#/definitions/User'
  *       responses:
  *           200:
  *               description: Sucesfull login
@@ -143,6 +182,7 @@ async function hashPassword(user) {
  */
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', function (err, user, info) {
+    console.log(user)
     console.log(user, err, req.session);
     if (err) { return next(err); }
     if (!user) {
@@ -170,6 +210,35 @@ router.post('/logout', (req, res) => {
   req.logout();
   req.res.status(200).send({ msg: '' });
 });
+/**
+ * @swagger
+ * /users/comment:
+ *   post:
+ *       descripton: Returns list of stops in path and total distance.
+ *       content: 
+ *          application/x-www-form-urlencoded
+ *       parameters:
+ *          - in: body
+ *            name: aa
+ *            schema:
+ *              $ref: '#/definitions/User'
+ *       responses:
+ *           200:
+ *               description: Sucesfull login
+ *           401:
+ *               description: Cant login
+ *               content:
+ *                   application/json:
+ *               schema:
+ *                   type: object
+ */
+router.post('/comment',passport.authenticate('moderator',{}), async (req, res,next) => {
+  console.log(req.body)
+  res.status(401).send({ message: "Wrong credits" })
+});
+
+
+
 class User {
 
   constructor(name, email, password) {
@@ -181,11 +250,35 @@ class User {
   }
 }
 class Comment {
-  constructor(userId,forumId,content,attachments) {
+  constructor(userId, forumId, text, attachments) {
     this.userId = userId;
     this.forumId = forumId;
-    this.content = content;
+    this.text = text;
     this.attachements = attachements
+  }
+}
+class Page {
+  constructor(url, pageId, categories) {
+    this.url = url;
+    this.pageId = pageId;
+    this.categories = categories;
+  }
+}
+class Forum {
+  constructor(pageId) {
+    this.pageId = pageId;
+    this.commentCount = 0;
+    this.filesCount = 0;
+    this.lastUpdate = 0;
+    this.lastCommentId = "";
+    this.createTime = new Date();
+  }
+}
+class Log {
+  constructor(actionType,message){
+    this.actionType = actionType;
+    this.message = message;
+    this.createTime = new Date();
   }
 }
 export { router };
